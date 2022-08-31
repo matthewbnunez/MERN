51fbdb008910ee263f252e20106bc4c929ca90ec
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-08-31 10:54:26 -0700

    initial commit

--- React/prop_it_up/src/components/PersonCard.jsx
+++ React/prop_it_up/src/components/PersonCard.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 class PersonCard  extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
     render() {
-        const { firstName, lastName, age, hairColor } = this.props;
+        const { firstName, lastName, hairColor } = this.props;
+        const ageIncrase = () => {
+            this.setState({ age: this.state.age + 1})
+        }
         return (
             <div>
                 <h1>{ firstName }, { lastName }</h1>
-                <p>Age: { age }</p>
+                <p>Age: { this.state.age }</p>
                 <p>Hair Color: { hairColor }</p>
+                <button onClick={ageIncrase}>Birthday Button for { firstName } { lastName }</button>
             </div>
         );
     }
