3f0083c3fd3d1fed7e04069a44ffd8adf5f709d2
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-09-13 14:03:18 -0700

    err response added

--- Full_Stack_MERN/productManager/server/controllers/product.controller.js
+++ Full_Stack_MERN/productManager/server/controllers/product.controller.js
@@ -15,29 +15,29 @@ module.exports.createProduct = (request, response) => {
         description
     })
         .then(product => response.json(product))
-        .catch(err => response.json(err));
+        .catch(err => response.status(400).json(err));
 }
 
 module.exports.getAllProduct = (request, response) => {
     Product.find({})
         .then(product => response.json(product))
-        .catch(err => response.json(err))
+        .catch(err => response.status(400).json(err))
 }
 
 module.exports.getProduct = (request, response) => {
     Product.findOne({_id:request.params.id})
         .then(product => response.json(product))
-        .catch(err => response.json(err))
+        .catch(err => response.status(400).json(err))
 }
 
 module.exports.updateProduct = (request, response) => {
     Product.findOneAndUpdate({_id: request.params.id}, request.body, {new:true})
         .then(updatedProduct => response.json(updatedProduct))
-        .catch(err => response.json(err))
+        .catch(err => response.status(400).json(err))
 }
 
 module.exports.deleteProduct = (request, response) => {
     Product.deleteOne({ _id: request.params.id })
         .then(deleteProduct => response.json(deleteProduct))
-        .catch(err => response.json(err))
+        .catch(err => response.status(400).json(err))
 }
