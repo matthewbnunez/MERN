dba995d92f0d636586a71ebc4bd7d227b1cfdec9
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-09-12 17:49:01 -0700

    added Detail Page

--- Full_Stack_MERN/productManager/client/src/App.js
+++ Full_Stack_MERN/productManager/client/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Main from './views/Main';
+import Detail from './views/Detail';
+
 
 function App() {
     return (
-        <div className="App">
-            <Main />
-        </div>
+    <div className="App">
+        <Routes>
+            <Route element={<Main/>} path="/" />
+            <Route element={<Detail/>} path="/api/product/:id" />
+        </Routes>                         
+    </div>
     );
 }
 export default App;
