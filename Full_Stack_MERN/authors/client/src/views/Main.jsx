467d69ff131bddbc558964b7f29b608ff60bdf22
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2022-09-13 23:17:33 -0700

    initial commit

--- Full_Stack_MERN/authors/client/src/views/Main.jsx
+++ Full_Stack_MERN/authors/client/src/views/Main.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import PersonForm from '../components/PersonForm';
 import PersonList from '../components/PersonList';
-import { Link } from 'react-router-dom';
 
 
 const Main = (props) => {
